19f06a848c3a80c217e2586eefeab53803f8d04d
David Yack <dyack@coloradotc.com>
2016-02-22 23:48:19 -0700

    updated to 8.0.2 binaries

--- CTCBulkOpLib/_CreateNewNuGetPackage/Config.ps1
+++ CTCBulkOpLib/_CreateNewNuGetPackage/Config.ps1
@@ -16,7 +16,7 @@
 # Specify the Version Number to use for the NuGet package. If not specified, the version number of the assembly being packed will be used.
 # NuGet version number guidance: https://docs.nuget.org/docs/reference/versioning and the Semantic Versioning spec: http://semver.org/
 # e.g. "" (use assembly's version), "1.2.3" (stable version), "1.2.3-alpha" (prerelease version).
-$versionNumber = "2.0.0-alpha3"
+$versionNumber = ""
 
 # Specify any Release Notes for this package. 
 # These will only be included in the package if you have a .nuspec file for the project in the same directory as the project file.
@@ -24,7 +24,7 @@ $releaseNotes = ""
 
 # Specify a specific Configuration and/or Platform to only create a NuGet package when building the project with this Configuration and/or Platform.
 #	e.g. $configuration = "Release"
-#		 $platform = "Any CPU"
+#		 $platform = "AnyCPU"
 $configuration = ""
 $platform = ""
 
